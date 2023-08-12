7fb5930b6dba8ff87186893f3d063fef25d91ab9
Mateus Andriola <mateusandriola18@gmail.com>
2023-08-12 19:42:21 -0300

    feat(config joken in test)

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,8 @@ config :access_managment, AccessManagment.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+config :joken, default_signer: "test"
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :access_managment, AccessManagmentWeb.Endpoint,
